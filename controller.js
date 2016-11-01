1449a158481d255da44fced6f1ca7a235c1f1a9b
Luis Felipe Holguin <luis@farleyetc.com>
2016-11-01 09:55:28 -0500

    control atributes implemnted

---
+++ controller.js
@@ -0,0 +1,18 @@
+var vHeight = 280;
+var vWidth = 200;
+var pHeight = 100;
+var pWidht = 100;
+
+$(document).ready(function () {
+    $('#video').css({
+        height: vHeight + 'px',
+        width: vWidth + 'px',
+    
+    });
+    $('#the-canvas').css({
+        width: pWidht + 'px',
+        height: pHeight + 'px'
+    });
+   
+    
+});
